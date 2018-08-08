7cee20af1c36631558fce8d10bfdada6152d5baf
satichun <32406329+satichun@users.noreply.github.com>
2018-08-07 20:55:08 -0400

    Create stock-wdc.js

---
+++ stock-wdc.js
@@ -0,0 +1,14 @@
+function initializeViz() {
+  // JS object that points at empty div in the html
+  var placeholderDiv = document.getElementById("tableauViz");
+  // URL of the viz to be embedded
+  var url = "https://public.tableau.com/profile/satish.c#!/vizhome/StockPricewithWDC-WIP/AppleStock";
+  // An object that contains options specifying how to embed the viz
+  var options = {
+    width: '600px',
+    height: '600px',
+    hideTabs: true,
+    hideToolbar: true,
+  };
+  viz = new tableau.Viz(placeholderDiv, url, options);
+}
