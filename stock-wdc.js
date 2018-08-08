9cbd6d92fae8a128773dd8d177c14fd5870e7812
satichun <32406329+satichun@users.noreply.github.com>
2018-08-08 00:03:46 -0400

    Update stock-wdc.js

--- stock-wdc.js
+++ stock-wdc.js
@@ -2,7 +2,7 @@ function initializeViz() {
   // JS object that points at empty div in the html
   var placeholderDiv = document.getElementById("tableauViz");
   // URL of the viz to be embedded
-  var url = "https://public.tableau.com/views/StockPricewithWDC-WIP/AppleStock?:embed=y&:display_count=yes"";
+  var url = "https://public.tableau.com/views/StockPricewithWDC-WIP/AppleStock?:embed=y&:display_count=yes";
   // An object that contains options specifying how to embed the viz
   var options = {
     width: '600px',
